c609851594109ac2c0a4f504594cbd5c01a27a36
khooinguyeen <khoinguyenmai17102005@gmail.com>
2021-12-17 17:37:54 +0700

    updated

--- Sign Language Translator/HandTracking.py
+++ Sign Language Translator/HandTracking.py
@@ -33,7 +33,7 @@ while True:
     cTime = time.time()
     fps = 1/(cTime-pTime)
     pTime = cTime
-    print(fps)
+    # print(fps)
     # cv2.putText(img, str (int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
