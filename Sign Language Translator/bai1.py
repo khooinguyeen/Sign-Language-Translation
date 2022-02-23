60bc20f94936dddb65d1503df5a45de2e2affcbd
khooinguyeen <khoinguyenmai17102005@gmail.com>
2022-02-23 20:11:31 +0700

    update new model with 50 sentences

--- Sign Language Translator/bai1.py
+++
@@ -1,8 +0,0 @@
-a=input("Nhập xâu S: ");
-b=list(a);
-b.reverse();
-b=''.join(b);
-if a==b:
-    print("Xâu đối xứng!");
-else:
-    print("Xâu không đối xứng!");
--- Sign Language Translator/bai3.py
+++
@@ -1,1 +0,0 @@
-print(input("Enter S: ").replace('anh', 'em'))
